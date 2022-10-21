b28385c78b5ad07f081ba68e4324913559309f3e
Eduardo_sena <ejmachacon3@gmail.com>
2022-10-21 07:45:28 -0500

    formato de codigo de visual

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
-
-    <?php include ('menu.php'); ?>
-    <div class="Contenedor_principal"> 
+<?php include('menu.php'); ?>
+<div class="Contenedor_principal">
 
     <div class="carrousel">
         <div class="grande">
@@ -15,25 +14,25 @@
             <li class="punto"></li>
         </ul>
     </div>
-        
 
-            <div class="Video_Mapa">
-                
-                        <!--Inicio video-->
 
-            <video class="contenedor-video" controls autoplay>
-                <source src="videos/intro.mp4">
-            </video>
+    <div class="Video_Mapa">
+
+        <!--Inicio video-->
+
+        <video class="contenedor-video" controls autoplay>
+            <source src="videos/intro.mp4">
+        </video>
         <!--Fin video-->
-            <!--Inicio de mini-mapa-->
-            <iframe class="mini_mapa" height="100%" width="46%" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d248.56078636063208!2d-74.07002752896227!3d4.5989342536791105!2m3!1f0!2f0!3f0!3m2!
+        <!--Inicio de mini-mapa-->
+        <iframe class="mini_mapa" height="100%" width="46%" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d248.56078636063208!2d-74.07002752896227!3d4.5989342536791105!2m3!1f0!2f0!3f0!3m2!
                 1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f99a5c94d12f3%3A0x38edfa51ffd15636!2sLavanderia%20Megarapido!5e0!3m2!1ses!2sco!4v1665500763282!5m2!1ses!2sco">
-            </iframe>
-            <!--Fin de mini-mapa-->
+        </iframe>
+        <!--Fin de mini-mapa-->
 
-            </div>
-        
     </div>
+
+</div>
 </div>
 
 <?php include('pie_de_pagina.php'); ?>
